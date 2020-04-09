df1f0e87d3ee9f15c18e3b989171c0731a2f51b8
Gardel Souza <gardel@gardelsouza.net>
2020-04-09 09:45:34 -0300

    restruturaçao do projeto

--- site/api/Database/ILiteDbContext.cs
+++
@@ -1,9 +0,0 @@
-using LiteDB;
-
-namespace api.Database
-{
-    public interface ILiteDbContext
-    {
-        LiteDatabase Database { get; }
-    }
-}
--- site/api/Database/LiteDbOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace api.Database
-{
-    public class LiteDbOptions
-    {
-        public string DatabaseLocation { get; set; }
-    }
-}
